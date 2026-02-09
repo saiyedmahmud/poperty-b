9b263cb16f1d6a47265ac22b02ac31f094981bb8
saiyedmahmud <saiyedmahmud05@gmail.com>
2026-02-09 13:06:47 +0600

    Add FloorFactory for generating floor model instances

---
+++ database/factories/FloorFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Building;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Floor>
+ */
+class FloorFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'buildingId' => Building::factory(),
+            'floorNumber' => $this->faker->numberBetween(1, 10),
+        ];
+    }
+}
